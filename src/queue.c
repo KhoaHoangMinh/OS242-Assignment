f2d612ed336245b330ab431bbfb9145ee8082bba
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-04-06 13:40:20 +0700

    Change to FIFO

--- src/queue.c
+++ src/queue.c
@@ -20,20 +20,13 @@ struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	if (q != NULL && q->size > 0) {
-		struct pcb_t * highest = q->proc[0];
-		int index = 0;
-		for (int i = 1; i < q->size; i++) {
-			if (q->proc[i]->priority > highest->priority) {
-				highest = q->proc[i];
-				index = i;
-			}
-		}
-		for (int i = index; i < q->size - 1; i++) {
-			q->proc[i] = q->proc[i + 1];
-		}
+	if (!empty(q)) {
+		struct pcb_t * proc = q->proc[0];
+		for (int i = 0; i < q->size - 1; i++) {
+            q->proc[i] = q->proc[i + 1];
+        }
 		q->size--;
-		return highest;
+		return proc;
 	}
 	return NULL;
 }
