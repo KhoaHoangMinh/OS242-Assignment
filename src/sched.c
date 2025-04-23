3e8141ead1245945cb5acb31a0d0986aa659f07a
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-04-23 22:24:52 +0700

    Add proc to running queue

--- src/sched.c
+++ src/sched.c
@@ -93,21 +93,29 @@ struct pcb_t * get_proc(void) {
 }
 
 void put_proc(struct pcb_t * proc) {
+	printf("inside put_proc\n");
 	proc->ready_queue = &ready_queue;
 	proc->mlq_ready_queue = mlq_ready_queue;
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
+	pthread_mutex_lock(&queue_lock);
+	enqueue(&running_list, proc);
+	pthread_mutex_unlock(&queue_lock);
 
 	return put_mlq_proc(proc);
 }
 
 void add_proc(struct pcb_t * proc) {
+	printf("inside add_proc\n");
 	proc->ready_queue = &ready_queue;
 	proc->mlq_ready_queue = mlq_ready_queue;
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
+	pthread_mutex_lock(&queue_lock);
+	enqueue(&running_list, proc);
+	pthread_mutex_unlock(&queue_lock);
 
 	return add_mlq_proc(proc);
 }
